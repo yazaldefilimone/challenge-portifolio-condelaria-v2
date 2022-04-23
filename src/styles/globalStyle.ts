b4f7089a573b1b1ef02b73b2124b33ad8f2d2ec4
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-24 00:05:23 +0200

    feat: create global styles

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,97 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  :root {
+    --frist-color: #00d2df;
+    --frist-color-alt: #eccb39;
+    --dark-color: #121212;
+    --dark-color-alt:#151515;
+    --second-color: #15181f;
+    --gray-color:#c4c4c4;
+    --white-color:#fff;
+    --header-height:3rem;
+
+  --body-font: "Inter", Arial, sans-serif;
+  --h1-font-size: 1.5rem;
+  --h2-font-size: 1.25rem;
+  --h3-font-size: 1rem;
+  --normal-font-size: .938rem;
+  --small-font-size: .813rem;
+  --smaller-font-size: .75rem;
+
+  /*========== Font weight ==========*/
+  --font-medium: 500;
+  --font-semi-bold: 600;
+
+  /*========== z index ==========*/
+  --z-tooltip: 10;
+  --z-fixed: 100;
+}
+
+@media screen and (min-width: 968px) {
+  :root {
+    --h1-font-size: 2.25rem;
+    --h2-font-size: 1.5rem;
+    --h3-font-size: 1.25rem;
+    --normal-font-size: 1rem;
+    --small-font-size: .875rem;
+    --smaller-font-size: .813rem;
+  }
+}
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: var(--body-font);
+  font-size: var(--normal-font-size);
+  background-color: var(--body-color);
+  color: var(--white-color);
+}
+
+h1, h2, h3 {
+  color: var(--white-color);
+  font-weight: var(--font-semi-bold);
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+/*=============== REUSABLE CSS CLASSES ===============*/
+.container {
+  max-width: 1024px;
+  padding-right: 1.5rem;
+  padding-left: 1.5rem;
+  margin:0 auto;
+}
+
+.grid {
+  display: grid;
+  gap: 1.5rem;
+}
+
+.section {
+  padding: 4.5rem 0 2rem;
+}
+main {
+  overflow: hidden;
+}
+
+`;
